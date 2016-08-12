7f0e30af5cf1f7c6754c78071c7b7079d2070c0d
Dennis Koenig <dennis.koenig@comparon.de>
2016-08-12 16:49:50 +0200

    Remove unused namespaces

--- Facade/TaskProcessorFacade.php
+++ Facade/TaskProcessorFacade.php
@@ -4,19 +4,13 @@ namespace Comparon\SchedulingBundle\Facade;
 
 use Comparon\SchedulingBundle\Model\TaskConfiguration;
 use Cron\CronExpression;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-
 use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class TaskProcessorFacade
 {
-    public static function process(
-        ContainerAwareCommand $command,
-        TaskConfiguration $config
-    ) {
+    public static function process(ContainerAwareCommand $command, TaskConfiguration $config)
+    {
         if (self::isDue($config)) {
             $args = implode(' ', $config->getParameters());
             $process = new Process('./console ' . $command->getName() . ' ' . $args);
