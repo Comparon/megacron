4cc0a72d54c80029e27cf386a8aeaee601e65596
Thoralf Schillat <thoralf.schillat@comparon.de>
2016-08-12 16:42:17 +0200

    task execution is now asynchronous

--- Command/MegacronCommand.php
+++ Command/MegacronCommand.php
@@ -3,15 +3,12 @@
 namespace Comparon\SchedulingBundle\Command;
 
 use Comparon\SchedulingBundle\Facade\TaskProcessorFacade;
-use Comparon\SchedulingBundle\Model\TaskConfiguration;
 use Comparon\SchedulingBundle\Model\TaskInterface;
-use Cron\CronExpression;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Doctrine\Common\Annotations\Reader;
 
 class MegacronCommand extends ContainerAwareCommand
 {
@@ -27,27 +24,13 @@ class MegacronCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $now = new \DateTime('now');
         foreach ($this->getApplication()->all() as $command) {
             if ($command instanceof TaskInterface) {
                 $configs = $command->getTaskConfigurations();
                 foreach ($configs as $config) {
-                    if ($this->isDue($config, $now)) {
-                        TaskProcessorFacade::process($output, $command, $config);
-                    }
+                    TaskProcessorFacade::process($command, $config);
                 }
             }
         }
-    }
-
-    private function isDue(TaskConfiguration $taskConfiguration, \DateTime $now)
-    {
-        $expression = $taskConfiguration->getCronExpression();
-        if (CronExpression::isValidExpression($expression)) {
-            $cron = CronExpression::factory($expression);
-            return $cron->isDue($now);
-        }
-        // TODO: Log
-        return false;
     }
 }
--- Facade/TaskProcessorFacade.php
+++ Facade/TaskProcessorFacade.php
@@ -3,27 +3,35 @@
 namespace Comparon\SchedulingBundle\Facade;
 
 use Comparon\SchedulingBundle\Model\TaskConfiguration;
+use Cron\CronExpression;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 class TaskProcessorFacade
 {
     public static function process(
-        OutputInterface $output,
         ContainerAwareCommand $command,
         TaskConfiguration $config
     ) {
-        $input = new ArrayInput($config->getParameters());
-        try {
-            $output->writeln($command->getName());
-            $returnCode = $command->execute($input, $output);
-            echo $returnCode;
-        } catch (\Exception $e) {
-
-            $returnCode = -1;
-            //TODO LOG
+        if (self::isDue($config)) {
+            $args = implode(' ', $config->getParameters());
+            $process = new Process('./console ' . $command->getName() . ' ' . $args);
+            $process->run();
         }
-        return $returnCode;
+    }
+
+    private static function isDue(TaskConfiguration $taskConfiguration)
+    {
+        $expression = $taskConfiguration->getCronExpression();
+        if (CronExpression::isValidExpression($expression)) {
+            $cron = CronExpression::factory($expression);
+            return $cron->isDue(new \DateTime('now'));
+        }
+        // TODO: Log
+        return false;
     }
 }
