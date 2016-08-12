67adc87f2320ea283ce3cd6a8b88d4e9039297ee
Thoralf Schillat <thoralf.schillat@comparon.de>
2016-08-12 15:40:06 +0200

    forced execute-method to be public

--- Model/TaskInterface.php
+++ Model/TaskInterface.php
@@ -2,10 +2,19 @@
 
 namespace Comparon\SchedulingBundle\Model;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 interface TaskInterface
 {
     /**
      * @return TaskConfiguration[]
      */
     public function getTaskConfigurations();
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    public function execute(InputInterface $input, OutputInterface $output);
 }
