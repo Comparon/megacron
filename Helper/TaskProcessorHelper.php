2c8b1ac8d7e4b3e3ded0991d11eead11bbdb88a4
Thoralf Schillat <thoralf.schillat@comparon.de>
2016-08-15 17:19:16 +0200

    renamed TaskProcessorFacade to TaskProcessorHelper

--- Helper/TaskProcessorHelper.php
+++ Helper/TaskProcessorHelper.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Comparon\SchedulingBundle\Facade;
+namespace Comparon\SchedulingBundle\Helper;
 
 use Comparon\SchedulingBundle\Model\TaskConfiguration;
 use Cron\CronExpression;
 use Symfony\Component\Console\Command\Command;;
 use Symfony\Component\Process\Process;
 
-class TaskProcessorFacade
+class TaskProcessorHelper
 {
     /** @var string */
     private $binDirPath;
@@ -32,41 +32,62 @@ class TaskProcessorFacade
 
     public function process()
     {
-        $pidFileDir = $this->binDirPath
-            . '..' . DIRECTORY_SEPARATOR
-            . 'var' . DIRECTORY_SEPARATOR
-            . 'comparon_scheduling' . DIRECTORY_SEPARATOR;
-
-        if (!file_exists($pidFileDir)) {
-            if (!mkdir($pidFileDir)) {
-                throw new Exception("Could not create directory {$pidFileDir}");
-            }
-        }
+        $pidFileDir = $this->getPidFileDir();
+        $this->createDir($pidFileDir);
 
         $processHash = sha1($this->command->getName() . $this->taskConfig->getCronExpression());
         $pidFilePath = $pidFileDir . $processHash . '.pid';
 
         if($this->isDue()) {
-            $args = implode(' ', $this->taskConfig->getParameters());
-            $processCmd = trim($this->binDirPath . 'console ' . $this->command->getName() . ' ' . $args);
-            $processCmdSuffix = ' > /dev/null 2>/dev/null &';
-            if (file_exists($pidFilePath) && $this->taskConfig->isWithOverlapping()) {
-                unlink($pidFilePath);
+            $args = '';
+            if (count($this->taskConfig->getParameters()) > 0) {
+                $args = implode(' ', $this->taskConfig->getParameters());
             }
 
-            if (!$this->taskConfig->isWithOverlapping()) {
-                if (file_exists($pidFilePath)) {
+            $processCmd = $this->binDirPath . 'console ' . $this->command->getName() . ' ' . $args;
+            $processCmdSuffix = ' > /dev/null 2>/dev/null &';
+
+            if (file_exists($pidFilePath)) {
+                if ($this->taskConfig->isWithOverlapping()) {
+                    unlink($pidFilePath);
+                } else {
                     $pid = intval(file_get_contents($pidFilePath));
                     $result = shell_exec("ps -fp {$pid}");
                     if(strpos($result, $processCmd) !== false) {
                         return;
                     }
                 }
+            }
 
+            if (!$this->taskConfig->isWithOverlapping()) {
                 file_put_contents($pidFilePath, '');
                 $processCmdSuffix .= ' echo $! >> ' . $pidFilePath;
             }
+            
             shell_exec($processCmd . $processCmdSuffix);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getPidFileDir()
+    {
+        return $this->binDirPath . '..'
+            . DIRECTORY_SEPARATOR . 'var'
+            . DIRECTORY_SEPARATOR . 'comparon_scheduling'
+            . DIRECTORY_SEPARATOR
+        ;
+    }
+
+    /**
+     * @param string $dirPath
+     * @throws \Exception
+     */
+    private function createDir($dirPath)
+    {
+        if (!file_exists($dirPath) && !mkdir($dirPath)) {
+            throw new \Exception("Could not create directory {$dirPath}");
         }
     }
 
@@ -80,7 +101,7 @@ class TaskProcessorFacade
             $cron = CronExpression::factory($expression);
             return $cron->isDue(new \DateTime('now'));
         }
-        // TODO: Log
+        // TODO: implement logging
         return false;
     }
 }
